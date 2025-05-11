0fa8ff04231919392583318ff02469d4180bab5d
Cchanoknun <chanoknun.c@codium.co>
2025-05-11 17:56:52 +0700

    Added Facebook link to NavBar

--- components/Navbar.js
+++ components/Navbar.js
@@ -22,19 +22,26 @@ export default function Navbar() {
       `}
     >
       <div className="max-w-7xl mx-auto px-4 py-3 flex items-center justify-between">
-        {/* Logo */}
+        {/* Left: Logo */}
         <div className="flex items-center space-x-3">
-          <img src="/images/logo.png" alt="Logo" className="w-20 h-20 md:w-24 md:h-24" /> 
+          <img src="/images/logo.png" alt="Logo" className="w-16 h-16 md:w-20 md:h-20" />
           <span className="text-xl font-bold text-blue-900">Kitti Construction</span>
         </div>
 
-        {/* Navigation Links */}
-        <div className="space-x-6 text-blue-800 font-semibold hidden md:flex">
+        {/* Center: Navigation Links */}
+        <div className="flex-1 flex justify-center space-x-6 text-blue-800 font-semibold">
           <Link href="/">หน้าแรก</Link>
           <Link href="/about">เกี่ยวกับเรา</Link>
           <Link href="/projects">โครงการ</Link>
           <Link href="/gallery">แกลเลอรี่</Link>
           <Link href="/contact">ติดต่อเรา</Link>
+        </div>
+
+        {/* Right: Facebook Logo */}
+        <div className="ml-4">
+          <a href="https://www.facebook.com/profile.php?id=100057677932751" target="_blank" rel="noopener noreferrer">
+            <img src="/images/facebook-new.png" alt="Facebook Logo" className="w-8 h-8" />
+          </a>
         </div>
       </div>
     </nav>
