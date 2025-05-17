a4a1443945b8da5be62350266c428bba20e3de1a
Cchanoknun <chanoknun.cho@gmail.com>
2025-05-17 21:22:59 +0700

    Fix footer and tel number ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 // pages/_app.js
-import '../styles/globals.css' // โหลด CSS หลักของ Tailwind
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
