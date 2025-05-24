3a35ab1a3bdd85b18deed3152c29d31b516359cf
Cchanoknun <chanoknun.cho@gmail.com>
2025-05-24 13:28:04 +0700

    Add progress loading

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
 // pages/_app.js
 import '../styles/globals.css'
+import '../styles/nprogress-custom.css'
+import NProgress from 'nprogress'
+import Router from 'next/router'
+
+
+
+// ตั้งค่าถ้าต้องการ (ไม่บังคับ)
+NProgress.configure({ showSpinner: false, speed: 400 })
+
+// Event สำหรับเริ่มโหลด
+Router.events.on('routeChangeStart', () => {
+  NProgress.start()
+})
+
+// Event เมื่อโหลดเสร็จ
+Router.events.on('routeChangeComplete', () => {
+  NProgress.done()
+})
+
+// ถ้าเกิด error
+Router.events.on('routeChangeError', () => {
+  NProgress.done()
+})
 
 function MyApp({ Component, pageProps }) {
   return (
