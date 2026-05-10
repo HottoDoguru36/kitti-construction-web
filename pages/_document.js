4eefb1e08c46547e58eea38db2530b018d7484d5
Cchanoknun <chanoknun.cho@gmail.com>
2026-05-10 16:39:10 +0700

    featL add google meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,10 @@ export default function Document() {
         <link rel="icon" href="/favicon.svg?v=3" type="image/svg+xml" />
         <link rel="shortcut icon" href="/favicon.svg?v=3" />
         <link rel="apple-touch-icon" href="/favicon.svg?v=3" />
+        <meta
+          name="google-site-verification"
+          content="BtihpSb676LyuPgdN1uz4nijflu7Lj6Jfbnmgn73UTU"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@400;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
